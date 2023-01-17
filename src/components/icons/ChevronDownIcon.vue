d5211d00176dcceac5aa4894c0dda410e914a7fd
Daniel Justin <danieljmuleke@gmail.com>
2023-01-17 21:51:35 +0200

    Implemented chevron down icon component

---
+++ src/components/icons/ChevronDownIcon.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import ChevronUpIcon from './ChevronUpIcon.vue'
+
+export default defineComponent({
+  components: { ChevronUpIcon },
+  name: "ChevronDownIcon",
+  props: {
+    color:  String,
+    size: Number,
+  },
+})
+</script>
+
+<template>
+  <chevron-up-icon :size="size" :color="color" class="rotate-180" />
+</template>
