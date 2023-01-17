f07edbcfa38c1d48a4ebee43267e09ce5a4143e8
Daniel Justin <danieljmuleke@gmail.com>
2023-01-17 21:50:51 +0200

    Implemented chevron up icon component

---
+++ src/components/icons/ChevronUpIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none">
+    <path :fill="color" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "ChevronUpIcon",
+  props: {
+    color: {
+      type: String,
+      default: '#343434'
+    },
+    size: {
+      type: Number,
+      default: 24
+    }
+  },
+})
+</script>
