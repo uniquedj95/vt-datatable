1e08347216667210d75a5d1901f93ddaea49e2d3
Daniel Justin <danieljmuleke@gmail.com>
2023-01-17 13:53:01 +0200

    Added tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="mb-4 text-3xl font-bold text-orange-600 underline">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
